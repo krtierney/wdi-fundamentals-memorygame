23b917d7f03ddc8c70d17e15e32f596b4f784741
Kaitlyn Tierney <kaitlyn.means@gmail.com>
2016-05-19 17:57:51 +0100

    Initial update containing game logic and basic html and css layout

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,8 @@
+//Confirm that JS is working in web console
 console.log("JS file is connected to HTML! Woo!")
+
+//Create variables for four cards and assign values 
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
