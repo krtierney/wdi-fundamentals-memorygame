648e81ffeb5282af3e45228eb3045da752ef43f7
Kaitlyn Tierney <kaitlyn.means@gmail.com>
2016-05-23 12:52:37 +0100

    added function to shuffle starting cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,8 @@
-//Confirm that JS is working in web console
-// alert('JS file is connected to HTML! Woo!')
-
 //Define variable 'cards' containing an array
 var cards = ['queen', 'queen', 'king', 'king'];
+
+//call Fisher-Yates function to shuffle cards in the array
+fisherYates(cards);
 
 //Define an empty array to hold the cards in play
 var cardsInPlay = [];
@@ -26,9 +26,9 @@ var createBoard = function() {
     //execute isTwoCards function when a card is clicked
     cardElement.addEventListener('click', isTwoCards);
   }
-
 };
 
+//displays card face image
 var flipCard = function() {
   if (this.getAttribute('data-card') === 'queen') {
     this.innerHTML = '<img src="queen.png" alt="Queen" />';
@@ -40,12 +40,11 @@ var flipCard = function() {
 var isTwoCards = function() {
   //add clicked card to an array of cards in play
   cardsInPlay.push(this);
-//check for match when two cards are clicked
+//call isMatch function when two cards are clicked
   if (cardsInPlay.length === 2) {
     //delay alert until after card image is displayed
     setTimeout(function() { 
       isMatch(cardsInPlay);
-
       //clear cards in play for next try
       cardsInPlay[0].innerHTML = '';
       cardsInPlay[1].innerHTML = '';
@@ -54,14 +53,26 @@ var isTwoCards = function() {
   }
 };
 
+//check for match between clicked cards
 var isMatch = function() {
   if (cardsInPlay[0].getAttribute('data-card') === cardsInPlay[1].getAttribute('data-card')) {
     alert('You found a match!');
   } else {
     alert('Try again.');
   }
-
 };
 
-createBoard();
+//Fisher-Yates shuffle to randomize card values
+function fisherYates (myArray) {
+  var i = myArray.length;
+  if ( i == 0 ) return false;
+  while ( --i ) {
+     var j = Math.floor( Math.random() * ( i + 1 ) );
+     var tempi = myArray[i];
+     var tempj = myArray[j];
+     cards[i] = tempj;
+     cards[j] = tempi;
+   }
+}
 
+createBoard();
